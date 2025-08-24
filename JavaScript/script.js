0889279b8a720f18a983b36968a2c5275d483f13
maxjohn <haririvo81@gmail.com>
2025-08-25 01:24:11 +0300

    mon projet

---
+++ JavaScript/script.js
@@ -0,0 +1,107 @@
+let textimportant = document.querySelector(".textContent");
+let texte = "Passionné par l'informatique";
+let liste_textes = [...texte];
+let i = 0;
+
+setInterval(() => {
+  if (i < liste_textes.length) {
+    textimportant.textContent += liste_textes[i];
+    i += 1;
+  } else {
+    textimportant.textContent = "";
+    i = 0;
+  }
+}, 150);
+
+// Barre de navigation
+let boxiz = document.querySelector(".boxiz");
+let nav = document.querySelector(".nav");
+boxiz.addEventListener("click", () => {
+  nav.classList.toggle("addClass");
+});
+
+let photo1 = document.querySelector(".photo1");
+let photo2 = document.querySelector(".photo2");
+
+let j = 0;
+setInterval(() => {
+  j += 1;
+  if (j == 2) {
+    photo1.style.transform = "translate(-265px)";
+    photo2.style.transform = "translate(-260px)";
+  }
+
+  if (j > 4) {
+    setTimeout(() => {
+      photo1.style.transform = "translate(0)";
+      photo2.style.transform = "translate(-0)";
+    }, 500);
+  }
+
+  if (j > 4) {
+    j = 0;
+  }
+}, 2000);
+
+let photo3 = document.querySelector(".photo3");
+let photo4 = document.querySelector(".photo4");
+let k = 0;
+
+setInterval(() => {
+  k += 1;
+  if (k == 2) {
+    photo3.style.transform = "translate(-265px)";
+    photo4.style.transform = "translate(-260px)";
+  }
+
+  if (k > 4) {
+    setTimeout(() => {
+      photo3.style.transform = "translate(0)";
+      photo4.style.transform = "translate(-0)";
+    }, 500);
+  }
+
+  if (k > 4) {
+    k = 0;
+  }
+}, 1500);
+
+let navBar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+  if (scrollY > 50) {
+    navBar.classList.add("addclass1");
+  } else {
+    navBar.classList.remove("addclass1");
+  }
+});
+
+let boxs = document.querySelectorAll(".box");
+let skills = document.querySelectorAll(".skills")
+let textImportant =document.querySelector(".textImportant");
+let etude_superieur = document.querySelector(".etude_superieur");
+let hackathon = document.querySelector(".hackathon");
+let marquant = document.querySelector(".marquant");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("addClass2");
+      } else {
+        entry.target.classList.remove("addClass2");
+      }
+    });
+  },
+  { threshold: 0.3 }
+);
+boxs.forEach((box) => {
+  observer.observe(box);
+});
+skills.forEach((skill)=>{
+  observer.observe(skill);
+})
+observer.observe(textImportant);
+observer.observe(etude_superieur);
+observer.observe(marquant);
+observer.observe(hackathon);
